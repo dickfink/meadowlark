90e0a4f877ae01ac29de20815e00987d7cd9350f
Dick Fink <dickfink@gmail.com>
2017-01-25 01:43:51 +0000

    Added a form and body-parser which prints formdata to console

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -1,20 +1,34 @@
 var express = require("express");
 var app = express();
 var fortune = require('./lib/fortune.js')
-
+var bodyParser  = require('body-parser');
 // set up handlebars view engine
 var handlebars = require('express-handlebars').create({ defaultLayout:'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-
+//set static folder
+app.use(express.static(__dirname + '/public'));
+// set up body-parser
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+// set up server
 app.set("port", process.env.PORT || 3000);
 
+// routes
 app.get('/', function(req, res){
     res.render('home');
 });
 
 app.get('/about', function(req, res){
     res.render('about', {fortune: fortune.getFortune});
+});
+
+
+// form posting
+app.post('/', function(req, res){
+   console.log('Quote (from querystring):' + req.body.goodThing);
+   res.redirect(303, '/');
 });
 
 // custom 404 page
